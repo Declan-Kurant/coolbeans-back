e1badd3f029e5f9571fb2de1498fd0ae23942cd1
Sarah Yu <sarahyu0692@gmail.com>
2017-12-15 16:20:31 -0500

    add first seed

--- db/seeds.js
+++ db/seeds.js
@@ -1,2 +1,28 @@
 const Coffee = require('./schema')
 const FlavorProfile = require('./schema')
+const Coffee = require('./schema')
+
+const IndonesiaFlavorProfile = new FlavorProfile({
+	body: 'Full',
+	acidity: 'Low',
+	notes: {
+		isFruity: true,
+		isSour: false,
+		isVeggie: true,
+		isRoasted: false,
+		isSpicy: false,
+		isNuttyCocoa: true,
+		isSweet: false,
+		isFloral: false,
+		isWeird: false
+	}
+})
+
+const Indonesia = new Coffee({
+	name: 'Indonesia',
+	region: 'India & The Pacific',
+	country: 'Indonesia',
+	imgUrl: '#',
+	description: '#',
+	flavorProfile: [IndonesiaFlavorProfile]
+})
