6abd90efc22b21732a0bf23e958933c1602f0009
Michael Ortiz <ortiz_michael@bah.com>
2017-12-20 10:57:32 -0500

    fix connection again

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/cool-beans', { useMongoClient: true })
+if (process.env.NODE_ENV == 'production') {
+	mongoose.connect(process.env.MLAB_URL)
+} else {
+	mongoose.connect('mongodb://localhost/cool-beans', { userMongoClient: true })
+}
 
 mongoose.Promise = Promise
 
